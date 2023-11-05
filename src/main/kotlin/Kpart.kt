69061e7c472ac85251f1b3bb88e581177dc5de85
heziblack <heziblackforfun@gmail.com>
2023-11-05 17:18:27 +0800

    新建文件夹的更新

--- src/main/kotlin/Kpart.kt
+++ src/main/kotlin/Kpart.kt
@@ -7,10 +7,16 @@ import net.mamoe.mirai.message.data.content
 
 /** 盒子的模块接入入口(监听) */
 class HeziListener:ListenerHost{
+    var keyWordList:List<String> = listOf()
     @EventHandler
     suspend fun groupsListen(event: GroupMessageEvent){
-        if (event.message.content == "盒子"){
-            event.group.sendMessage("hello Members")
+        val mt = event.message.content
+        if (mt !in keyWordList) return
+        when(event.message.content){
+
         }
     }
 }
+
+
+
---
+++ src/main/kotlin/game/core/GameCore.kt
@@ -0,0 +1,14 @@
+package com.example.game.core
+
+/** 游戏核心主体主要功能：
+ * 识别玩家指令合法性
+ * 管理并维护游戏列表
+ * 根据指令操作游戏
+ * 以字符串文或图片形式输出操作结果(img\String\null)
+ * 存储游戏信息
+ * 读取自定义地图
+ * */
+object GameCore {
+
+}
+
